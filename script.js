01a00f0af83adf36b701113202c3cefc63975f58
saran1522 <90507409+saran1522@users.noreply.github.com>
2022-11-08 14:43:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let dispEl_1=document.getElementById("disp-el-1")
+let dispEl_2=document.getElementById("disp-el-2")
+let count1=0,count2=0
+
+function increment_1a(){
+    count1+=1
+    dispEl_1.textContent=count1
+}
+function increment_2a(){
+    count1+=2
+    dispEl_1.textContent=count1
+}
+function increment_3a(){
+    count1+=3
+    dispEl_1.textContent=count1
+}
+
+function increment_1b(){
+    count2+=1
+    dispEl_2.textContent=count2
+}
+function increment_2b(){
+    count2+=2
+    dispEl_2.textContent=count2
+}
+function increment_3b(){
+    count2+=3
+    dispEl_2.textContent=count2
+}
